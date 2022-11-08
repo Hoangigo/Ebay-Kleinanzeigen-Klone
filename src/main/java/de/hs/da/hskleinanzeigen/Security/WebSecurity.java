100cb19fe1381e1524b4a9278d9acfac9925d322
Moise C <moise.j.chengo@stud.h-da.de>
2022-11-08 10:45:19 +0100

    fixed error("No beans of HTTPSecurity") by adding new annotation

--- src/main/java/de/hs/da/hskleinanzeigen/Security/WebSecurity.java
+++ src/main/java/de/hs/da/hskleinanzeigen/Security/WebSecurity.java
@@ -3,6 +3,7 @@ package de.hs.da.hskleinanzeigen.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurity {
 
   @Bean
