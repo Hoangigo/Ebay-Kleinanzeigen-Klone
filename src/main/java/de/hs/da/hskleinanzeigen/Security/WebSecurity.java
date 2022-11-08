1829e4ab0b3134ce0c3b01d530a67d30f234d6d8
Moise C <moise.j.chengo@stud.h-da.de>
2022-11-08 10:35:09 +0100

    use a new way to secure app. WebSecurityConfigurerAdapter is deprecated

--- src/main/java/de/hs/da/hskleinanzeigen/Security/WebSecurity.java
+++ src/main/java/de/hs/da/hskleinanzeigen/Security/WebSecurity.java
@@ -1,30 +1,33 @@
 package de.hs.da.hskleinanzeigen.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurity {
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .withUser("user")
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withUsername("user")
         .password("{noop}user") //don't encode password
         .roles("Benutzer")
-        .and()
-        .withUser("admin")
+        .build();
+    UserDetails admin = User.withUsername("admin")
         .password("{noop}admin")
         .roles("Administrator")
-    ;
+        .build();
+    return new InMemoryUserDetailsManager(admin, user);
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests()
         .antMatchers("/actuator/health", "/actuator/info").permitAll()
         .antMatchers("/api/advertisements").hasAnyRole("Benutzer", "Administrator")
@@ -33,12 +36,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         .and()
         .httpBasic()
     ;
-    //we have stopped the csrf to make post method work
-    http.cors()
-        .and()
-        .csrf()
-        .disable()
-    ;
+    return http.build();
   }
-
 }
