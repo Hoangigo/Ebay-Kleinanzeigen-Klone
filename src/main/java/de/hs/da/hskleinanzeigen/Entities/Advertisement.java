566949328b312be217ee4e33058f721e02d1ca92
Moise C <moise.j.chengo@stud.h-da.de>
2022-11-20 11:56:08 +0100

    added Named Query for searching advertisements

--- src/main/java/de/hs/da/hskleinanzeigen/Entities/Advertisement.java
+++ src/main/java/de/hs/da/hskleinanzeigen/Entities/Advertisement.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -18,6 +19,51 @@ import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "AD")
+
+@NamedQuery(name = "Advertisement.findByTypeCategoryPriceToPriceFrom",
+    query = "SELECT a FROM Advertisement a where a.type = :t AND a.category.id = :c "
+        + "AND a.price >= :pf AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByTypeCategoryPriceTo",//
+    query = "SELECT a FROM Advertisement a where a.type = :t AND a.category.id = :c "
+        + " AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByTypeCategoryPriceFrom",//
+    query = "SELECT a FROM Advertisement a where a.type = :t AND a.category.id = :c "
+        + "AND a.price >= :pf ")
+@NamedQuery(name = "Advertisement.findByTypeCategory",//
+    query = "SELECT a FROM Advertisement a where a.type = :t AND a.category.id = :c ")
+@NamedQuery(name = "Advertisement.findByTypePriceToPriceFrom",//
+    query = "SELECT a FROM Advertisement a where a.type = :t  "
+        + "AND a.price >= :pf AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByTypePriceTo",//
+    query = "SELECT a FROM Advertisement a where a.type = :t  "
+        + " AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByTypePriceFrom",//
+    query = "SELECT a FROM Advertisement a where a.type = :t  "
+        + "AND a.price >= :pf ")
+@NamedQuery(name = "Advertisement.findByType",//
+    query = "SELECT a FROM Advertisement a where a.type = :t ")
+@NamedQuery(name = "Advertisement.findByCategoryPriceToPriceFrom",//
+    query = "SELECT a FROM Advertisement a where  a.category.id = :c "
+        + "AND a.price >= :pf AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByCategoryPriceTo",//
+    query = "SELECT a FROM Advertisement a where  a.category.id = :c "
+        + " AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByCategoryPriceFrom",//
+    query = "SELECT a FROM Advertisement a where  a.category.id = :c "
+        + "AND a.price >= :pf ")
+@NamedQuery(name = "Advertisement.findByCategory",//
+    query = "SELECT a FROM Advertisement a where  a.category.id = :c ")
+@NamedQuery(name = "Advertisement.findByPriceToPriceFrom",//
+    query = "SELECT a FROM Advertisement a where  "
+        + " a.price >= :pf AND a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByPriceTo",//
+    query = "SELECT a FROM Advertisement a where  "
+        + "  a.price <= :pt")
+@NamedQuery(name = "Advertisement.findByPriceFrom",//
+    query = "SELECT a FROM Advertisement a where  "
+        + " a.price >= :pf ")
+
+
 public class Advertisement {
 
   public Advertisement() {
