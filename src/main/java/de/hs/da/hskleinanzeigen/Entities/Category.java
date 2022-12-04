1290ce90223adf4846612ad054c4e2178cbad3cc
Moise C <moise.j.chengo@stud.h-da.de>
2022-12-04 19:55:44 +0100

    remove JSON and Validation annotations, add Lombok annotations for getter and setter

--- src/main/java/de/hs/da/hskleinanzeigen/Entities/Category.java
+++ src/main/java/de/hs/da/hskleinanzeigen/Entities/Category.java
@@ -1,71 +1,31 @@
 package de.hs.da.hskleinanzeigen.Entities;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "CATEGORY")
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @EqualsAndHashCode.Include
   private Integer id;
 
   @Column(nullable = true)
-  @JsonAlias("parentId")
-  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private Integer parent_id;
 
-  @JsonProperty(namespace = "name")
-  @NotEmpty(message = "you must give a name to the new category")
+  @Column(nullable = true)
   private String name;
 
-  public Category(Integer id, Integer parent_id, String name) {
-    this.id = id;
-    this.parent_id = parent_id;
-    this.name = name;
-  }
-
-  public Category() {
-
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public Integer getParent_id() {
-    return parent_id;
-  }
-
-  public void setParent_id(Integer parent_id) {
-    this.parent_id = parent_id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public String toString() {
-    return "Category{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        '}';
-  }
 }
