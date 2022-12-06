52f8d84dc657dd305cbab214c6fe4cc90a70dec6
Moise C <moise.j.chengo@stud.h-da.de>
2022-12-06 10:03:48 +0100

    add general description of our Api by creating an OpenApi Bean

--- src/main/java/de/hs/da/hskleinanzeigen/HsKleinanzeigenApplication.java
+++ src/main/java/de/hs/da/hskleinanzeigen/HsKleinanzeigenApplication.java
@@ -1,12 +1,25 @@
 package de.hs.da.hskleinanzeigen;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HsKleinanzeigenApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HsKleinanzeigenApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(HsKleinanzeigenApplication.class, args);
+  }
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI() //
+        .info(new Info().title("HS Kleinanzeigen") //
+            .description("Plattform zum Verwalten von Kleinanzeigen")
+            .contact(new Contact().email("moise.j.chengo@stud.h-da.de"))
+            .version("v2.0.22"));
+  }
 }
